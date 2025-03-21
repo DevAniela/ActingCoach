39a68dd38d25f70f9a8246c6abd2cdc260bdef61
Ana Vi <dismal.damsel@gmail.com>
2025-03-21 13:09:23 +0200

    Added abstract User class

--- src/main/java/com/anavi/actingcoach/User.java
+++ src/main/java/com/anavi/actingcoach/User.java
@@ -10,10 +10,46 @@ package com.anavi.actingcoach;
  */
 public abstract class User {
     //ATRIBUTE
+    private String name, email, password;
     
     //CONSTRUCTOR
+    public User() {
+        this.name = "";
+        this.email = "";
+        this.password = "";
+    }
     
-    //METODE ABSTRACTE
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
     
     //GETTERI SI SETTERI
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    //METODE ABSTRACTE
+    public abstract void authenticate(); //implementare in subclase
 }
