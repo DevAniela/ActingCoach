ed7018654f25db7622180bac88631e30932b18f3
Ana Vi <dismal.damsel@gmail.com>
2025-03-25 16:11:33 +0200

    Added JournalEntry class

---
+++ src/main/java/com/anavi/actingcoach/JournalEntry.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.anavi.actingcoach;
+
+/**
+ *
+ * @author Ana Vi
+ */
+public class JournalEntry {
+    //ATRIBUTE
+    
+    
+    //CONTRUCTORI
+    
+    
+    //GETTERI SI SETTERI
+    
+    
+    //METODE
+    
+}
