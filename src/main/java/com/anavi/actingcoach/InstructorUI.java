86958f8b59da6675864f9ba5a316c2941c63538d
Ana Vi <dismal.damsel@gmail.com>
2025-04-18 15:58:03 +0300

    Set up basic menu and placeholders for session management for InstructorUI class.

--- src/main/java/com/anavi/actingcoach/InstructorUI.java
+++ src/main/java/com/anavi/actingcoach/InstructorUI.java
@@ -13,11 +13,46 @@ public class InstructorUI extends ActingCoachUI {
 
     @Override
     public void displayMenu() {
-        // TODO: display menu options
+        System.out.println("\n=== Main Menu ===");
+        System.out.println("1. View Sessions");
+        System.out.println("2. Add Feedback");
+        System.out.println("3. Add Evaluation");
+        System.out.println("4. Cancel Session");
+        System.out.println("0. Exit");
     }
 
     @Override
     public void handleInput() {
-        // TODO: handle user input
+        Scanner scanner = new Scanner(System.in);
+
+        boolean running = true;
+
+        while (running) {
+            displayMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("TO DO: View sessions");
+                    break;
+                case 2:
+                    System.out.println("TO DO: Add feedback");
+                    break;
+                case 3:
+                    System.out.println("TO DO: Add evaluation");
+                    break;
+                case 4:
+                    System.out.println("TO DO: Cancel session");
+                    break;
+                case 0:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Not a valid option");
+            }
+
+        }
+
     }
 }
